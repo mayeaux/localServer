ad85e64b02de27437ab0b1b133e6c05b45a447c1
Anthony <contact@anthonymayfield.com>
2019-07-02 18:16:24 -0600

    making server

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+const fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
